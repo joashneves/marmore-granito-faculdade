e49472b8577c9dcfc6aacbc6fe4e5b6a8f4b61a9
Joash <141419406+joashneves@users.noreply.github.com>
2023-09-15 23:53:24 -0300

    Codigo comentado e colocado o Readme

--- Program.cs
+++ Program.cs
@@ -6,8 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Linq.Expressions;
-using System.Threading;
+
 
 class Program
 {
@@ -17,7 +16,7 @@ class Program
     public static void Main(string[] args)
     {
         //Variaveis do sistema
-        char escolha;
+        int escolha;
         int id;
         int numero;
         double medidas;
@@ -29,16 +28,16 @@ class Program
 
 
         // Criar 10 produtos com valores diferentes
-        CriarProduto(01, 12345, 2.2, "Bloco 1", "Mármore", 50.0, 75.0, "Pedreira A");
-        CriarProduto(02, 67890, 3.3, "Bloco 2", "Granito", 60.0, 80.0, "Pedreira B");
-        CriarProduto(03, 11111, 1.5, "Bloco 3", "Mármore", 45.0, 70.0, "Pedreira A");
-        CriarProduto(04, 22222, 2.8, "Bloco 4", "Granito", 55.0, 85.0, "Pedreira C");
-        CriarProduto(05, 33333, 1.2, "Bloco 5", "Mármore", 40.0, 60.0, "Pedreira B");
-        CriarProduto(06, 44444, 4.2, "Bloco 6", "Granito", 70.0, 90.0, "Pedreira A");
-        CriarProduto(07, 55555, 2.5, "Bloco 7", "Mármore", 48.0, 72.0, "Pedreira C");
-        CriarProduto(08, 66666, 3.7, "Bloco 8", "Granito", 65.0, 90.0, "Pedreira B");
-        CriarProduto(09, 77777, 2.3, "Bloco 9", "Mármore", 42.0, 68.0, "Pedreira A");
-        CriarProduto(10, 88888, 3.4, "Bloco 10", "Granito", 58.0, 78.0, "Pedreira C");
+        CriarProduto(1, 12345, 2.2, "Bloco 1", "Mármore", 50.0, 75.0, "Pedreira A");
+        CriarProduto(2, 67890, 3.3, "Bloco 2", "Granito", 60.0, 80.0, "Pedreira B");
+        CriarProduto(3, 11111, 1.5, "Bloco 3", "Mármore", 45.0, 70.0, "Pedreira A");
+        CriarProduto(4, 22222, 2.8, "Bloco 4", "Granito", 55.0, 85.0, "Pedreira C");
+        CriarProduto(5, 33333, 1.0, "Bloco 5", "Mármore", 40.0, 60.0, "Pedreira B");
+        CriarProduto(6, 44444, 4.2, "Bloco 6", "Granito", 70.0, 100.0, "Pedreira A");
+        CriarProduto(7, 55555, 2.5, "Bloco 7", "Mármore", 48.0, 72.0, "Pedreira C");
+        CriarProduto(8, 66666, 3.7, "Bloco 8", "Granito", 65.0, 90.0, "Pedreira B");
+        CriarProduto(9, 77777, 2.0, "Bloco 9", "Mármore", 42.0, 68.0, "Pedreira A");
+        CriarProduto(10, 88888, 3.0, "Bloco 10", "Granito", 58.0, 78.0, "Pedreira C");
 
         while (true)
         {
@@ -50,43 +49,34 @@ class Program
 3 – Buscar Bloco por código 
 4 – Listar Blocos por pedreira 
 5 - SAIR");
-
-
+            
+            // Pede o usuario para escolhe uma opção
             Console.Write("Escolha uma opção: ");
-            escolha = Console.ReadLine()[0]!;
+            escolha = verificarNumInt(Console.ReadLine());
 
-            if (!string.IsNullOrWhiteSpace(escolha)) //Verifica se o console está vazio, caso esteja não inicia a linha devido ao "!"
+            // Entra em um novela de casos
+            switch (escolha)
             {
-                switch (escolha)
-                {
-                    case '1':
-                        CadastrarBloco();
-                        Console.WriteLine("Produto Cadastrado com sucesso!");
-                        break;
-                    case '2':
-                        AcessarProdutosAnteriores();
-                        Console.WriteLine("Fim da lista!");
-                        break;
-                    case '3':
-                        BuscarBlocoCodigo();
-                        break;
-                    case '4':
-                        BuscarBlocoPedreira();
-                        break;
-                    case '5':
-                        Console.WriteLine("Encerrando o programa");
-                        return;
-                    default:
-                        Console.WriteLine("Opção invalida");
-                        break;
-
-                }
-            } 
-
-            else
-            {
-                Console.WriteLine("Entrada inválida. Pressione qualquer tecla para continuar.");
-                Console.ReadKey();
+                case 1: // caso o usuario digite 1 ele entra na função de cadastrar bloco
+                    CadastrarBloco();
+                    Console.WriteLine("Produto Cadastrado com sucesso!");
+                    break;
+                case 2: // caso o usuario digite 2 ele entra na função de lista produtos
+                    AcessarProdutosAnteriores();
+                    Console.WriteLine("Fim da lista!");
+                    break;
+                case 3: // caso o usuario digite 3 ele entra na função de buscar produtos por codigo
+                    BuscarBlocoCodigo();
+                    break;
+                case 4: // caso o usuario digite 4 ele entra na função de listar produtos por pedreira
+                    BuscarBlocoPedreira();
+                    break;
+                case 5: // caso o usuario digite 5 ele ele returna a função saindo do switch 
+                    Console.WriteLine("Encerrando o programa");
+                    return;
+                default:
+                    Console.WriteLine("Opção invalida");
+                    break;
             }
         }
     }
@@ -155,9 +145,9 @@ class Program
             goto ESCOLHATIPO;
         }
 
-        Console.Write("Valor de compra: ");
+        Console.Write("Valor de compra: R$");
         valorCompra = verificarNumDouble(Console.ReadLine()); //define valor da compra
-        Console.Write("Valor de Venda: ");
+        Console.Write("Valor de Venda: R$");
         valorVenda = verificarNumDouble(Console.ReadLine()); // define valor da venda
 
     ESCOLHATIPOPEDREIRA:
@@ -187,21 +177,6 @@ class Program
             goto ESCOLHATIPOPEDREIRA;
         }
 
-        //Imprime na tela o que foi adicionado com uso de delay
-        Console.WriteLine($"Número: {numero}");
-        Thread.Sleep(500);
-        Console.WriteLine($"Medidas: {medidas}");
-        Thread.Sleep(500);
-        Console.WriteLine($"Descrição: {descricao}");
-        Thread.Sleep(500);
-        Console.WriteLine($"Tipo: {tipo}");
-        Thread.Sleep(500);
-        Console.WriteLine($"Valor de Compra: {valorCompra}");
-        Thread.Sleep(500);
-        Console.WriteLine($"Valor de Venda: {valorVenda}");
-        Thread.Sleep(500);
-        Console.WriteLine($"Pedreira: {pedreira}"); 
-
 
         // Cria o produto com os valores cadastrado
         CriarProduto(id,
@@ -220,14 +195,14 @@ class Program
     public static void AcessarProdutosAnteriores()
     {
         Console.WriteLine("Produtos Anteriores:");
-        Console.WriteLine("ID|    NUMERO   | MEDIDAS |  DESCRICAO |    TIPO    | VALOR DA COMPRA |  VALOR DA VENDA |   PEDREIRA");
+        Console.WriteLine("ID | NUMERO | MEDIDAS |   DESCRICAO  |    TIPO     |  VALOR DA COMPRA | VALOR DA VENDA | PEDREIRA");
 
         //Percorre a lista de produtos
         foreach (Produto produto in listaDeProdutos)
         {
+
             // Acessa função do objeto
-            Console.Write($"{produto.getId()} |     {produto.getNumero()}   |    {produto.getMedidas()}  |   {produto.getDescricao()}  |   {produto.getTipo()}  |        {produto.getValorCompra()}       |        {produto.getValorVenda()}       |   {produto.getPedreira()} \n");
-            Thread.Sleep(500);
+            Console.Write($"{produto.getId()} |   {produto.getNumero()} |    {produto.getMedidas()}  |   {produto.getDescricao()}  |   {produto.getTipo()}  |    R${produto.getValorCompra()}    |      R${produto.getValorVenda()}      |   {produto.getPedreira()} \n");
         }
         Console.ReadKey();
     }
@@ -261,7 +236,7 @@ class Program
         Console.WriteLine("Escreva a pedreira do produto:");
         string pedreiraEncontrar;
         int escolhaTipo;
-    //Amarzena a variavel que o usuario quer procurar
+        //Amarzena a variavel que o usuario quer procurar
 
     ESCOLHATIPOPEDREIRA:
         Console.Write(@"Pedreira: 
@@ -289,7 +264,7 @@ class Program
             goto ESCOLHATIPOPEDREIRA;
         }
 
-        Console.WriteLine("ID | NUMERO | MEDIDAS | DESCRICAO | TIPO   |  VALOR DA COMPRA | VALOR DA VENDA | PEDREIRA");
+        Console.WriteLine("ID | NUMERO | MEDIDAS |   DESCRICAO  |    TIPO     |  VALOR DA COMPRA | VALOR DA VENDA | PEDREIRA");
 
         // Percorre a lista de produto que ja existe
         foreach (Produto produto in listaDeProdutos)
@@ -297,8 +272,8 @@ class Program
             if (produto.getPedreira() == pedreiraEncontrar)  //pedreiraEncontrar)
             {
                 // Acessa função do objeto
-                Console.Write($"{produto.getId()} |     {produto.getNumero()}   |    {produto.getMedidas()}  |   {produto.getDescricao()}  |   {produto.getTipo()}  |   {produto.getValorCompra()}  |   {produto.getValorVenda()}  |   {produto.getPedreira()} \n");
-                Thread.Sleep(500);
+                Console.Write($"{produto.getId()} |   {produto.getNumero()} |    {produto.getMedidas()}  |   {produto.getDescricao()}  |   {produto.getTipo()}  |    R${produto.getValorCompra()}    |      R${produto.getValorVenda()}      |   {produto.getPedreira()} \n");
+
             }
         }
 
@@ -309,23 +284,27 @@ class Program
     // Um Parse teimoso
     public static int verificarNumInt(string numero)
     {
-        int num;
-        while (true)
+        int num; // numero que vai ser retornado
+        // a função recebe uma string do Console.Read() e tenta passa para Int
+        while (true) // Entra em um loop so saindo caso ele passe a string para int
         {
             try
             {
+                //Ele tenta passar para int
                 num = int.Parse(numero);
-                return num;
+                return num; // caso consiga retorna o valor que foi colocado no Console.Read() e devolve como int
 
             }
-            catch
+            catch // Caso de um erro e ele não consiga passa para int ele entra nessa linha
             {
                 Console.WriteLine("numero invalido! ");
-                Console.WriteLine("Insira um novo numero:");
-                numero = Console.ReadLine();
+                Console.WriteLine("Insira um novo numero:"); // Mensagem ao usuario
+                numero = Console.ReadLine(); // pede novo numero
             }
         }
     }
+
+    // mesma coisa do verificarNumInt() só que devolve um double
     public static double verificarNumDouble(string numero)
     {
         double num;
--- README.md
+++ README.md
@@ -1,3 +1,61 @@
 # marmore-granito-faculdade
 Avaliação processual 3º bimestre do ano de 2023 - Sistemas de informação MULTIVIX
 Grupo: Diogo Lima, Gellyorge Marvila, Joas Neves, Paulo Roberto
+
+Este projeto foi desenvolvido como parte de um trabalho de faculdade. Ele consiste na criação de uma aplicação de console em C# para gerenciar o controle de blocos de mármore e granito para uma empresa fictícia chamada Max Rochas e Produtos Ornamentais. O objetivo principal do projeto é aplicar os conceitos e boas práticas de Engenharia de Software aprendidos no curso, abordando desde a análise de requisitos até a implementação funcional do sistema, proporcionando uma oportunidade prática de aplicar os conhecimentos adquiridos na disciplina.
+
+## Guia:
+
+-[Fucionalidades](#fucionalidades)
+    -[menu](#menu)
+    -[cadastro](#cadastro)
+    -[Lista](#Lista)
+    -[Buscar](#buscar-por-codigo)
+    -[Listar-Produto](#listar-por-pedreira)
+    -[Sair](#Sair)
+-[Erros](#Erros)
+
+## fucionalidades
+    Aqui estão as seções que explicam como o código funciona e como são mostradas pelo sistema.
+
+### menu
+    O menu aparece no console, mostrando 5 opções. O usuário digita um dos números exibidos na tela, que representa a opção subsequente. Por exemplo, ao digitar "1 - cadastrar", o usuário será levado à funcionalidade de cadastro.
+
+![imagem-menu](./img/menu.png)
+
+### cadastro
+    Funcionalidade que solicita as informações para cadastrar um novo bloco.
+
+![cadastrar](./img/cadastrar-1.png)
+
+    Neste exemplo, após o usuário pressionar "1" para entrar nas opções de cadastro, a primeira informação solicitada é o número do bloco.
+
+![cadastrar](./img/cadastrar-2.png)
+
+    Após escolher o tipo de bloco, o sistema pede informações como número, medidas e descrição. Em seguida, o usuário seleciona o tipo de material, que pode ser Mármore ou Granito, e continua fornecendo informações sobre o valor da compra e o valor da venda.
+
+![cadastrar](./img/cadastar-3.png)
+
+    Por fim, o sistema pergunta de qual pedreira o bloco foi retirado e oferece opções para o usuário escolher.
+
+### Lista
+    Se o usuário pressionar "2" no menu, ele entrará na lista dos produtos cadastrados.
+![lista](./img/lista-2.png)
+
+### Buscar por codigo
+    Se o usuário pressionar "3" no menu, ele terá a opção de inserir o código do produto que deseja procurar, o "ID". O código retornará todas as informações daquele ID específico.
+
+![codigo](./img/buscar-por-codigo-4.png)
+
+### Listar por pedreira
+    Se o usuário pressionar "4" no menu, ele será direcionado para escolher uma das opções de pedreira que estão na lista da imagem. Após selecionar uma das 3 pedreiras, ele verá uma lista de todos os blocos que foram retirados daquela pedreira.
+
+![pedreiraListar](./img/lista-por-pedreira.png)
+
+### Sair
+    Se o usuário pressionar "5", o programa se encerrará.
+
+## Erros
+   Se algum erro for encontrado, será exibida uma mensagem de alerta com os detalhes do problema.
+
+![numeroErro](./img/numero-invalido.png)
